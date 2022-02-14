debab34ca3fed7c6686d358b7949071138fd9519
KU4NG <1214966109@qq.com>
2022-02-14 16:34:44 +0800

    新增访问日志中间件

--- src/initialize/router.go
+++ src/initialize/router.go
@@ -3,6 +3,7 @@ package initialize
 import (
     "github.com/gin-gonic/gin"
     "go-admin-api-example/src/common"
+    "go-admin-api-example/src/middleware"
     "go-admin-api-example/src/routes"
     "log"
 )
@@ -14,6 +15,9 @@ func Router() *gin.Engine {
 
     // 创建一个干净的路由引擎
     r := gin.New()
+
+    // 中间件
+    r.Use(middleware.AccessLog) // 访问日志中间件
 
     // 创建路由组，增加 /api 前缀
     apiGroup := r.Group(common.Conf.Server.ApiPrefix)
